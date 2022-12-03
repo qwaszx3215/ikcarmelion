dee6d821c20ebd10600d12cc9f81e3f081c92912
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2022-12-03 20:48:02 +0600

    Update stylls.js

--- src/stylls.js
+++ src/stylls.js
@@ -2,13 +2,12 @@ import styled from "styled-components";
 
 export const Styls = styled.div`
   background: #a7a9b0;
-  background-image: url("exel.jpg");
+  background-image: url("hopeexe.jpeg");
   background-position: ;
   background-repeat: no-repeat;
   background-size: cover;
   height: 100vh;
   position: relative;}
-
   .container {
     width: 100%;
     display: flex;
@@ -18,40 +17,33 @@ export const Styls = styled.div`
     height: 100vh;
   }
  
-
   .office {
     width: 400px;
     background: #fff;
     border: 5px solid #4285f4;
     border-radius: 5px;
   }
-
   .office-holder {
     width: 80%;
     margin: auto;
     padding: 20px 0px;
   }
-
   .logo {
     width: 100%;
     text-align: center;
     float: left;
     margin-bottom: 2em;
   }
-
   .logo h1 {
     font-family: arial;
   }
-
   .verify {
     width: 70%;
     margin: auto;
   }
-
   .verify h4 {
     font-family: arial;
   }
-
   fieldset {
     width: 90%;
     float: right;
@@ -68,14 +60,12 @@ export const Styls = styled.div`
     padding: 0px 2px;
     margin: 0px;
   }
-
   .verify input[type="text"] {
     width: 100%;
     float: right;
     border: none;
     padding: 3px 10px;
   }
-
   .verify button {
     border: none;
     height: 40px;
@@ -86,21 +76,17 @@ export const Styls = styled.div`
     border-radius: 5px;
     color: #fff;
   }
-
   .email-to-verify {
     border: 1px solid #000;
     border-radius: 10px;
   }
-
   .form-holder {
     width: 100%;
     float: left;
   }
-
   .form-holder h3 {
     font-weight: 600;
   }
-
   .form-holder input[type="email"],
   input[type="password"] {
     width: 100%;
@@ -116,7 +102,6 @@ export const Styls = styled.div`
     align-items: center;
     justify-content: center;
   }
-
   .form-holder .btn-holder .btn {
     background: rgb(4, 109, 4);
     font-size: 15px;
@@ -128,24 +113,20 @@ export const Styls = styled.div`
     margin-top: 20px;
     margin-bottom: 20px;
   }
-
   .form-holder .btn-holder .btn:hover {
     cursor: pointer;
   }
-
   .checkbox {
     width: 100%;
     float: left;
     margin-top: 5px;
   }
-
   .checkbox label {
     font-family: arial;
     font-size: 13px;
     float: left;
     margin-left: 1em;
   }
-
   .hide {
     display: none;
   }
